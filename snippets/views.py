a5fba22b86f608d3d1a589959b8bc307db1be163
Samuel Gómez Balderas <sierra_034@outlook.com>
2019-08-27 10:04:00 -0500

    protected views

--- snippets/views.py
+++ snippets/views.py
@@ -2,12 +2,38 @@ from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 from rest_framework import generics
 
+# imports for ExampleView
+from rest_framework.authentication import BasicAuthentication, SessionAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 # Create your views here.
-class SnippetList(generics.ListCreateAPIView):
+class SnippetList(generics.ListAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+class SnippetCreate(generics.CreateAPIView):
+    authentication_classes = [BasicAuthentication, SessionAuthentication]
+    permission_classes = [IsAuthenticated]
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    authentication_classes = [BasicAuthentication, SessionAuthentication]
+    permission_classes = [IsAuthenticated]
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+class ExampleView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, format=None):
+        content = {
+            'user': str(request.user),
+            'auth': str(request.auth),
+        }
+
+        return Response(content)
     
